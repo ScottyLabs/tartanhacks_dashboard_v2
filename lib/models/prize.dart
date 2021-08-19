9e983ebc586c10b4425507236db10e7a9f7d2840
Joyce Hong <joyce.hong8@gmail.com>
2021-08-19 17:43:46 -0400

    updated prize model

--- lib/models/prize.dart
+++ lib/models/prize.dart
@@ -1,15 +1,28 @@
 class Prize{
-  final String id;
+  final String event; //objectid
   final String name;
   final String description;
+  final String eligibility;
+  final String provider; //objectid
+  final String winner; //objectid
 
-  Prize({this.id, this.name, this.description});
+
+  Prize(
+      {this.event,
+      this.name,
+      this.description,
+      this.eligibility,
+      this.provider,
+      this.winner});
 
   factory Prize.fromJson(Map<String, dynamic> parsedJson) {
     Prize newPrize = new Prize(
-      id: parsedJson['_id'],
+      event: parsedJson['event'],
       name: parsedJson['name'],
-      description: parsedJson['description']
+      description: parsedJson['description'],
+      eligibility: parsedJson['eligibility'],
+      provider: parsedJson['provider'],
+      winner: parsedJson['winner']
     );
     return newPrize;
   }
