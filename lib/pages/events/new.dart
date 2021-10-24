92ab4ba9b83710853a0c3769d5aa266e825fdd17
Terrance Lee <texeirareborn@MacBook-Pro-105.local>
2021-10-23 23:10:17 -0400

    rough draft for new events

--- lib/pages/events/new.dart
+++ lib/pages/events/new.dart
@@ -1,142 +1,230 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:thdapp/models/old/event_model.dart';
-import 'package:thdapp/V1/api.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import '../custom_widgets.dart';
-import 'package:date_time_picker/date_time_picker.dart';
+import 'index.dart';
 
 class NewEventScreen extends StatefulWidget {
-
-  final Event eventData;
-
-  NewEventScreen(
-      {Key key, this.eventData})
-      : super(key: key);
   @override
-  _NewEventScreenState createState() => new _NewEventScreenState(eventData: eventData);
+  _NewEventScreenState createState() => _NewEventScreenState();
 }
 
 class _NewEventScreenState extends State<NewEventScreen> {
 
-  SharedPreferences prefs;
+  String _eventName = "";
+  String _eventDesc = "";
+  String _eventUrl = "";
+  String _duration = "";
+  String _date = "";
+  bool isPresenting = false;
+  TextEditingController nameController = TextEditingController();
+  TextEditingController descController = TextEditingController();
+  TextEditingController linkController = TextEditingController();
+  TextEditingController durationController = TextEditingController();
+  TextEditingController dateController = TextEditingController();
 
-  Event eventData;
-
-  _NewEventScreenState({Key key, this.eventData});
-
-  bool isAdmin = false;
-  var dropdownValue = 'Zoom';
-  final idController = new TextEditingController();
-  final eventNameController = new TextEditingController();
-  final eventDescController = new TextEditingController();
-  final gcalLinkController = new TextEditingController();
-  final zoomLinkController = new TextEditingController();
-  final zoomIDController = new TextEditingController();
-  final zoomPasswordController = new TextEditingController();
-  final durationController = new TextEditingController();
-
-
-  String dateTime;
-
-  DateTime selectedDate = DateTime.now();
-
-  TimeOfDay selectedTime = TimeOfDay(hour: 00, minute: 00);
-
-  String unixTime = '';
-  int accessCode = 0;
-
-  String appBarTitle = 'Create New Event';
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
-  void initState() {
-    super.initState();
-    //getData();
+  void dispose(){
+    nameController.dispose();
+    descController.dispose();
+    linkController.dispose();
+    durationController.dispose();
+    dateController.dispose();
+    super.dispose();
   }
 
-
-  void saveData() async {
-    bool result;
-    if(eventData == null){
-      result = await addEvents(eventNameController.text, unixTime, eventDescController.text, "a", zoomLinkController.text, accessCode, "a", "a", durationController.text);
-    }else{
-      result = await editEvents(eventData.id, eventNameController.text, unixTime, eventDescController.text, "a", zoomLinkController.text, accessCode, "a", "a", durationController.text);
-    }
-    if (result == true) {
-      _showDialog('Your event was successfully saved!', 'Success', result);
-    }else{
-      _showDialog('There was an error. Please try again.', 'Error.', result);
-    }
-  }
-
-  void getDate (DateTime input){
-    unixTime = (input.toUtc().millisecondsSinceEpoch/1000).toInt().toString();
-  }
-
-  void _showDialog(String response, String title, bool result) {
-    // flutter defined function
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        // return object of type Dialog
-        return AlertDialog(
-          title: new Text(title),
-          content: new Text(response),
-          actions: <Widget>[
-            // usually buttons at the bottom of the dialog
-            new FlatButton(
-              child: new Text(
-                "OK",
-                style: new TextStyle(color: Colors.white),
-              ),
-              color: new Color.fromARGB(255, 255, 75, 43),
-              onPressed: () {
-
-                Navigator.of(context).pop();
-                if(result == true){
-                  Navigator.pop(context);
-
-                }
-              },
-            ),
-          ],
-        );
+  Widget _buildName() {
+    return TextFormField(
+      decoration: FormFieldStyle(context, "Even Name"),
+      style: Theme.of(context).textTheme.bodyText2,
+      controller: nameController,
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'Project name is required';
+        }
+        return null;
       },
+      onSaved: (String value) {
+        _eventName = value;
+      },
+    );
+  }
+
+
+  Widget _buildDesc() {
+    return TextFormField(
+      decoration: FormFieldStyle(context, "Event Description"),
+      style: Theme.of(context).textTheme.bodyText2,
+      controller: descController,
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'Project description is required';
+        }
+
+        return null;
+      },
+      onSaved: (String value) {
+        _eventDesc = value;
+      },
+    );
+  }
+
+  Widget _buildEventURL() {
+    return TextFormField(
+      decoration: FormFieldStyle(context, "Event Link"),
+      style: Theme.of(context).textTheme.bodyText2,
+      keyboardType: TextInputType.url,
+      controller: linkController,
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'URL is Required';
+        }
+        return null;
+      },
+      onSaved: (String value) {
+        _eventUrl = value;
+      },
+    );
+  }
+
+  Widget _buildDuration() {
+    return TextFormField(
+      decoration: FormFieldStyle(context, "Duration"),
+      style: Theme.of(context).textTheme.bodyText2,
+      controller: durationController,
+      keyboardType: TextInputType.url,
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'Duration is Required';
+        }
+        return null;
+      },
+      onSaved: (String value) {
+        _duration = value;
+      },
+    );
+  }
+
+  Widget _buildDate() {
+    return TextFormField(
+      decoration: FormFieldStyle(context, "Date"),
+      style: Theme.of(context).textTheme.bodyText2,
+      controller: dateController,
+      keyboardType: TextInputType.url,
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'Date is Required';
+        }
+        return null;
+      },
+      onSaved: (String value) {
+        _date = value;
+      },
+    );
+  }
+
+  Widget _buildPresentingLive() {
+    int initialVal = 1;
+    if (isPresenting) initialVal = 0;
+    return Container(
+        padding: new EdgeInsets.fromLTRB(0, 10, 0, 0),
+        child: Column(
+            children: <Widget>[
+              SizedBox(width: 20,),
+              Text('Presenting',
+                  textAlign: TextAlign.left,
+                  style: Theme.of(context).textTheme.headline4),
+              Text('Do you wish to present live at the expo? If not, you must submit a video.',
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.bodyText2),
+              Switch(
+                  activeColor: Theme.of(context).colorScheme.secondary,
+                  activeTrackColor: Theme.of(context).colorScheme.primary,
+                  inactiveTrackColor: Theme.of(context).colorScheme.onSurface,
+                  value: isPresenting,
+                  onChanged: (value) {
+                    setState(() {
+                      isPresenting = value;
+                    });
+                  }
+              )
+            ]
+        )
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    // if statement (if participant, return this... if admin, return with admin privileges)
     final mqData = MediaQuery.of(context);
     final screenHeight = mqData.size.height;
     final screenWidth = mqData.size.width;
+
     return Scaffold(
-        body: Container(
+        body:  Container(
             child: SingleChildScrollView(
                 child: ConstrainedBox(
-                    constraints: BoxConstraints(maxHeight: screenHeight),
+                    constraints: BoxConstraints(
+                        maxHeight: screenHeight
+                    ),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.end,
                       children: [
                         TopBar(backflag: true),
                         Stack(
                           children: [
-                            Column(children: [
-                              SizedBox(height: screenHeight * 0.05),
-                              CustomPaint(
-                                  size: Size(screenWidth, screenHeight * 0.75),
-                                  painter: CurvedTop(
-                                      color1: Theme.of(context)
-                                          .colorScheme
-                                          .secondaryVariant,
-                                      color2:
-                                      Theme.of(context).colorScheme.primary,
-                                      reverse: true)),
-                            ]),
+                            Column(
+                                children:[
+                                  SizedBox(height:screenHeight * 0.05),
+                                  CustomPaint(
+                                      size: Size(screenWidth, screenHeight * 0.75),
+                                      painter: CurvedTop(
+                                          color1: Theme.of(context).colorScheme.secondaryVariant,
+                                          color2: Theme.of(context).colorScheme.primary,
+                                          reverse: true)
+                                  ),
+                                ]
+                            ),
+                            Container(
+                                alignment: Alignment.center,
+                                padding: EdgeInsets.fromLTRB(0, 5, 0, 0),
+                                child: GradBox(
+                                    width: screenWidth*0.9,
+                                    height: screenHeight*0.75,
+                                    padding: EdgeInsets.fromLTRB(15, 5, 15, 5),
+                                    child: Form(
+                                        key: _formKey,
+                                        child: SingleChildScrollView(
+                                            child: Column(
+                                              crossAxisAlignment: CrossAxisAlignment.center,
+                                              children: [
+                                                Text("Create New Event", style: Theme.of(context).textTheme.headline2),
+                                                SizedBox(height:8),
+                                                _buildName(),
+                                                SizedBox(height:8),
+                                                _buildDesc(),
+                                                SizedBox(height:8),
+                                                _buildEventURL(),
+                                                SizedBox(height:8),
+                                                _buildDuration(),
+                                                SizedBox(height:8),
+                                                _buildDate(),
+                                                SolidButton(
+                                                    text: "Save Information"
+                                                ),
+                                              ],
+                                            )
+                                        )
+                                    )
+                                )
+                            )
                           ],
                         )
                       ],
-                    )))));
+                    )
+                )
+            )
+        )
+    );
   }
 }
