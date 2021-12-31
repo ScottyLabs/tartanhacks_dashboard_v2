1c9982044b172fa402e659cae2c0ad929de1dda5
Caroline Crooks <carolinecrooks3@gmail.com>
2021-12-30 19:37:52 -0500

    update api calls

---
+++ lib/models/teams.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+
+class Team {
+  final String teamID;
+  final bool visible;
+  final dynamic admin;
+  final String name;
+  final List<dynamic> members;
+  final String description;
+  
+
+  Team({
+    this.teamID, 
+    this.visible, 
+    this.admin, 
+    this.name, 
+    this.members, 
+    this.description});
+
+  factory Team.fromJson(Map<String, dynamic> parsedJson) {
+    return new Team(
+        teamID:  parsedJson["_id"],
+        visible: parsedJson["visible"],
+        admin: parsedJson["admin"],
+        name: parsedJson["name"],
+        members: parsedJson["members"],
+        description: parsedJson["description"]
+    );
+  }
+}
