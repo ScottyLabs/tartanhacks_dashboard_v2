f107144b91c0447c7a948da754b51eb2fa16d3f4
Anuda Weerasinghe <anuda@live.com>
2022-01-23 21:56:51 -0500

    replaced view profile with view schedule on home

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -14,6 +14,7 @@ import 'leaderboard.dart';
 import '../models/profile.dart';
 import '../models/team.dart';
 import 'checkin.dart';
+import 'events/index.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -29,7 +30,7 @@ class _HomeState extends State<Home> {
   Profile userData;
   Team userTeam;
 
-  void getData() async{
+  void getData() async {
     prefs = await SharedPreferences.getInstance();
 
     isAdmin = prefs.getBool('admin');
@@ -39,9 +40,7 @@ class _HomeState extends State<Home> {
     userData = await getProfile(id, token);
     userTeam = await getUserTeam(token);
 
-    setState(() {
-
-    });
+    setState(() {});
   }
 
   @override
@@ -57,182 +56,200 @@ class _HomeState extends State<Home> {
     final screenWidth = mqData.size.width;
     return Scaffold(
       body: Container(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget> [
-                TopBar(),
-                Stack(
-                  children: [
-                    Column(
-                        children:[
-                          SizedBox(height:screenHeight * 0.05),
-                          CustomPaint(
-                              size: Size(screenWidth, screenHeight * 0.75),
-                              painter: CurvedTop(color1: Theme.of(context).colorScheme.primary,
-                                  color2: Theme.of(context).colorScheme.secondaryVariant)
-                          ),
-                        ]
-                    ),
-                    if (userData == null)
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          SizedBox(height:30),
-                          Center(child: CircularProgressIndicator(color: Theme.of(context).colorScheme.secondary))
-                        ],
-                      )
-                    else
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Container(
-                              alignment: Alignment.topLeft,
-                              padding: EdgeInsets.fromLTRB(30, 0, 20, 0),
-                              child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text("HACKING TIME LEFT", style: Theme.of(context).textTheme.headline1),
-                                    SizedBox(height: 8),
-                                    CountdownTimer(
-                                      endTime: 1644954396000,
-                                      textStyle: TextStyle(
-                                          fontSize: 30.0,
-                                          fontWeight: FontWeight.bold,
-                                          color: Theme.of(context).colorScheme.secondary),
-                                    ),
-                                  ]
-                              )
-                          ),
-                          SizedBox(height: screenHeight * 0.08),
-                          Container(
-                              width: screenWidth,
-                              height: screenHeight * 0.10,
-                              alignment: Alignment.bottomCenter,
-                              padding: EdgeInsets.fromLTRB(0, 0, 0, 5),
-                              child: Text("Swipe to see all the places where the\n"
-                                  + String.fromCharCode($larr) + "hacking is happening" + String.fromCharCode(($rarr)),
-                                style: Theme.of(context).textTheme.bodyText1,
-                                textAlign: TextAlign.center,
-                              )
-                          ),
-                          CarouselSlider(
-                            items: [
-                              GradBox(
-                                  width: screenWidth*0.48,
-                                  height: screenWidth*0.48,
-                                  child: Column(
-                                      crossAxisAlignment: CrossAxisAlignment.center,
-                                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                      children: [
-                                        Text("Hi " + userData.firstName + " " + userData.lastName + "!", style: Theme.of(context).textTheme.headline4),
-                                        Text("Welcome to TartanHacks!", style: Theme.of(context).textTheme.bodyText2, textAlign: TextAlign.center,),
-                                        SolidButton(
-                                          text: "View Profile",
-                                          onPressed: () {
-                                            Navigator.push(
-                                              context,
-                                              MaterialPageRoute(builder: (context) =>
-                                                  ProfilePage()),
-                                            );
-                                          },
-                                        )
-                                      ]
-                                  )
-                              ),
-                              GradBox(
-                                  width: screenWidth*0.48,
-                                  height: screenWidth*0.48,
-                                  child: Column(
-                                      crossAxisAlignment: CrossAxisAlignment.center,
-                                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                      children: [
-                                        Text("Swag Points", style: Theme.of(context).textTheme.headline4),
-                                        Text("Points Earned: " + userData.totalPoints.toString(), style: Theme.of(context).textTheme.bodyText2),
-                                        SolidButton(
-                                          text: "Leaderboard",
-                                          onPressed: () {
-                                            Navigator.push(
-                                              context,
-                                              MaterialPageRoute(builder: (context) =>
-                                                  Leaderboard()),
-                                            );
-                                          },
-                                        ),
-                                        SolidButton(
-                                          text: "Check In",
-                                          onPressed: () {
-                                            Navigator.push(
-                                              context,
-                                              MaterialPageRoute(builder: (context) =>
-                                                  CheckIn()),
-                                            );
-                                          },
-                                        )
-                                      ]
-                                  )
-                              ),
-                              GradBox(
-                                  width: screenWidth*0.48,
-                                  height: screenWidth*0.48,
-                                  child: Column(
-                                      crossAxisAlignment: CrossAxisAlignment.center,
-                                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                      children: [
-                                        SolidButton(text: "Discord", onPressed: null,),
-                                        SolidButton(text: "Hopin", onPressed: null,),
-                                        SolidButton(text: "TH Website", onPressed: null,)
-                                      ]
-                                  )
-                              ),
-                            ],
-                            options: CarouselOptions(
-                              height: screenWidth*0.50,
-                              enlargeCenterPage: false,
-                              autoPlayCurve: Curves.fastOutSlowIn,
-                              enableInfiniteScroll: true,
-                              viewportFraction: 0.5,
+          child: Column(mainAxisAlignment: MainAxisAlignment.end, children: <
+              Widget>[
+        TopBar(),
+        Stack(
+          children: [
+            Column(children: [
+              SizedBox(height: screenHeight * 0.05),
+              CustomPaint(
+                  size: Size(screenWidth, screenHeight * 0.75),
+                  painter: CurvedTop(
+                      color1: Theme.of(context).colorScheme.primary,
+                      color2: Theme.of(context).colorScheme.secondaryVariant)),
+            ]),
+            if (userData == null)
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(height: 30),
+                  Center(
+                      child: CircularProgressIndicator(
+                          color: Theme.of(context).colorScheme.secondary))
+                ],
+              )
+            else
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                      alignment: Alignment.topLeft,
+                      padding: EdgeInsets.fromLTRB(30, 0, 20, 0),
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("HACKING TIME LEFT",
+                                style: Theme.of(context).textTheme.headline1),
+                            SizedBox(height: 8),
+                            CountdownTimer(
+                              endTime: 1644954396000,
+                              textStyle: TextStyle(
+                                  fontSize: 30.0,
+                                  fontWeight: FontWeight.bold,
+                                  color:
+                                      Theme.of(context).colorScheme.secondary),
                             ),
-                          ),
-                          SizedBox(height: 15),
-                          if (userTeam != null)
-                            GradBox(
-                              width: screenWidth*0.9,
-                              height: 60,
-                              onTap: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(builder: (context) =>
-                                      ProjSubmit()),
-                                );
-                              },
-                              child: Text(
-                                "VIEW YOUR PROJECT",
-                                style: Theme.of(context).textTheme.headline2,
-                              ),
-                            )
-                          else
-                            GradBox(
-                              width: screenWidth*0.9,
-                              height: 60,
-                              onTap: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(builder: (context) =>
-                                      TeamsList()),
-                                );
-                              },
-                              child: Text(
-                                "JOIN A TEAM",
-                                style: Theme.of(context).textTheme.headline2,
-                              ),
-                            )
-                        ],
-                      )
-                  ],
-                ),
-              ]
-          )
-      ),
+                          ])),
+                  SizedBox(height: screenHeight * 0.08),
+                  Container(
+                      width: screenWidth,
+                      height: screenHeight * 0.10,
+                      alignment: Alignment.bottomCenter,
+                      padding: EdgeInsets.fromLTRB(0, 0, 0, 5),
+                      child: Text(
+                        "Swipe to see all the places where the\n" +
+                            String.fromCharCode($larr) +
+                            "hacking is happening" +
+                            String.fromCharCode(($rarr)),
+                        style: Theme.of(context).textTheme.bodyText1,
+                        textAlign: TextAlign.center,
+                      )),
+                  CarouselSlider(
+                    items: [
+                      GradBox(
+                          width: screenWidth * 0.48,
+                          height: screenWidth * 0.48,
+                          child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Text(
+                                    "Hi " +
+                                        userData.firstName +
+                                        " " +
+                                        userData.lastName +
+                                        "!",
+                                    style:
+                                        Theme.of(context).textTheme.headline4),
+                                Text(
+                                  "Welcome to TartanHacks!",
+                                  style: Theme.of(context).textTheme.bodyText2,
+                                  textAlign: TextAlign.center,
+                                ),
+                                
+                                SolidButton(
+                                  text: "View Schedule",
+                                  onPressed: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => EventsHomeScreen()),
+                                    );
+                                  },
+                                )
+                              ])),
+                      GradBox(
+                          width: screenWidth * 0.48,
+                          height: screenWidth * 0.48,
+                          child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Text("Swag Points",
+                                    style:
+                                        Theme.of(context).textTheme.headline4),
+                                Text(
+                                    "Points Earned: " +
+                                        userData.totalPoints.toString(),
+                                    style:
+                                        Theme.of(context).textTheme.bodyText2),
+                                SolidButton(
+                                  text: "Leaderboard",
+                                  onPressed: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => Leaderboard()),
+                                    );
+                                  },
+                                ),
+                                SolidButton(
+                                  text: "Check In",
+                                  onPressed: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => CheckIn()),
+                                    );
+                                  },
+                                )
+                              ])),
+                      GradBox(
+                          width: screenWidth * 0.48,
+                          height: screenWidth * 0.48,
+                          child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                SolidButton(
+                                  text: "Discord",
+                                  onPressed: null,
+                                ),
+                                SolidButton(
+                                  text: "Hopin",
+                                  onPressed: null,
+                                ),
+                                SolidButton(
+                                  text: "TH Website",
+                                  onPressed: null,
+                                )
+                              ])),
+                    ],
+                    options: CarouselOptions(
+                      height: screenWidth * 0.50,
+                      enlargeCenterPage: false,
+                      autoPlayCurve: Curves.fastOutSlowIn,
+                      enableInfiniteScroll: true,
+                      viewportFraction: 0.5,
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  if (userTeam != null)
+                    GradBox(
+                      width: screenWidth * 0.9,
+                      height: 60,
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => ProjSubmit()),
+                        );
+                      },
+                      child: Text(
+                        "VIEW YOUR PROJECT",
+                        style: Theme.of(context).textTheme.headline2,
+                      ),
+                    )
+                  else
+                    GradBox(
+                      width: screenWidth * 0.9,
+                      height: 60,
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => TeamsList()),
+                        );
+                      },
+                      child: Text(
+                        "JOIN A TEAM",
+                        style: Theme.of(context).textTheme.headline2,
+                      ),
+                    )
+                ],
+              )
+          ],
+        ),
+      ])),
     );
   }
 }
