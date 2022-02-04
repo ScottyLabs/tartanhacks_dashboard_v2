e25b8159db75251e26ddce70b2f87d535cfb8777
Anuda Weerasinghe <anuda@live.com>
2022-02-04 17:51:56 -0500

    changed your qr code button link to check in page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -256,7 +256,7 @@ class _HomeState extends State<Home> {
                                     Navigator.push(
                                       context,
                                       MaterialPageRoute(
-                                          builder: (context) => QRPage()),
+                                          builder: (context) => CheckIn()),
                                     );
                                   },
                                 )
