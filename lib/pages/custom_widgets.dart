81ce41b413490bd3134d2a0b3be6f138d9da7fd5
Joyce Hong <joyce.hong8@gmail.com>
2022-04-02 15:29:29 -0400

    default page frame

--- lib/pages/custom_widgets.dart
+++ lib/pages/custom_widgets.dart
@@ -475,6 +475,60 @@ class TopBar extends StatelessWidget {
     );
   }
 }
+
+class DefaultPage extends StatelessWidget {
+  Widget child;
+  bool reverse;
+
+  DefaultPage({this.child, this.reverse=true})
+
+  @override
+  Widget build(BuildContext context) {
+    final mqData = MediaQuery.of(context);
+    final screenHeight = mqData.size.height;
+    final screenWidth = mqData.size.width;
+    return Scaffold(
+      body: Container(
+        child: SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              maxHeight: screenHeight
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TopBar(),
+                Stack(
+                  children: [
+                    Column(
+                      children: [
+                        SizedBox(height: screenHeight * 0.05),
+                        CustomPaint(
+                            size: Size(screenWidth, screenHeight * 0.75),
+                            painter: CurvedTop(
+                                color1: Theme.of(context).colorScheme.primary,
+                                color2: Theme.of(context).colorScheme.secondaryVariant,
+                                reverse: reverse
+                            )
+                        ),
+                      ]
+                    ),
+                    Container(
+                      padding: EdgeInsets.fromLTRB(screenWidth * 0.08, 0, screenWidth * 0.08, 0),
+                      height: screenHeight * 0.8,
+                      child: child
+                    )
+                  ]
+                )
+              ]
+            )
+          )
+        )
+      )
+    );
+  }
+}
+
 class WhiteOverlay extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
---
+++ qrcode-script.py
@@ -0,0 +1,27 @@
+import requests, qrcode, os
+
+url = "https://backend.tartanhacks.com/check-in"
+ciis = requests.get(url).json()
+names = list(map(lambda x: x['name'], ciis))
+ids = list(map(lambda x: x['_id'], ciis))
+
+path = os.path.join(os.getcwd(), "tartanhacks_qrcodes/")
+
+if (not os.path.exists(path)):
+	os.mkdir(path)
+
+	for i in range (len(ciis)):
+		qr = qrcode.QRCode(
+				version=1,
+				error_correction=qrcode.constants.ERROR_CORRECT_L,
+				box_size=10,
+				border=4,
+		)
+		qr.add_data(ids[i])
+		qr.make(fit=True)
+
+		img = qr.make_image(ids[i], fill_color=(7, 5, 76), back_color=(255, 199, 56))
+		img.save(path + names[i].replace("/", "") + ".jpg")
+		
+else:
+	print("delete tartanhacks_qrcodes folder")
