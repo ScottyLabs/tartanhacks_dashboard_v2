1ddaffd00e39f144cbb38402021ee50993afc50b
Gabriel Hall <gabrielhall@gabriels-mbp-8.wifi.local.cmu.edu>
2024-01-28 17:41:59 -0500

    make profile fields nullable

--- lib/models/profile.dart
+++ lib/models/profile.dart
@@ -2,91 +2,87 @@ class Profile {
   final int totalPoints;
   final String user;
   final String event; //objectid
-  final String email;
   final String firstName;
   final String lastName;
-  final int age;
+  final int? age;
   final String school;
-  final String college;
+  final String? college;
   final String level;
   final int graduationYear;
   final String gender;
-  final String genderOther;
+  final String? genderOther;
   final String ethnicity;
-  final String ethnicityOther;
+  final String? ethnicityOther;
   final String phoneNumber;
-  final String major;
-  final String coursework;
-  final String language;
-  final String hackathonExperience;
-  final String workPermission;
-  final String workLocation;
-  final String workStrengths;
-  final List sponsorRanking; //objectid
+  final String? major;
+  final String? coursework;
+  final String? language;
+  final String? hackathonExperience;
+  final String? workPermission;
+  final String? workLocation;
+  final String? workStrengths;
+  final List? sponsorRanking; //objectid
   final String github;
-  final String resume;
-  final String design;
-  final String website;
-  final List essays;
-  final List dietaryRestrictions;
-  final String shirtSize;
-  final bool wantsHardware;
-  final String address;
-  final String region;
+  final String? resume;
+  final String? design;
+  final String? website;
+  final List? essays;
+  final List? dietaryRestrictions;
+  final String? shirtSize;
+  final bool? wantsHardware;
+  final String? address;
+  final String? region;
   final String displayName;
-  String profilePicture;
+  final String? profilePicture;
 
-  Profile({
-      required this.totalPoints,
+  Profile(
+      {required this.totalPoints,
       required this.user,
       required this.event,
-      required this.email,
       required this.firstName,
       required this.lastName,
-      required this.age,
+      this.age,
       required this.school,
-      required this.college,
+      this.college,
       required this.level,
       required this.graduationYear,
       required this.gender,
-      required this.genderOther,
+      this.genderOther,
       required this.ethnicity,
-      required this.ethnicityOther,
+      this.ethnicityOther,
       required this.phoneNumber,
-      required this.major,
-      required this.coursework,
-      required this.language,
-      required this.hackathonExperience,
-      required this.workPermission,
-      required this.workLocation,
-      required this.workStrengths,
-      required this.sponsorRanking,
+      this.major,
+      this.coursework,
+      this.language,
+      this.hackathonExperience,
+      this.workPermission,
+      this.workLocation,
+      this.workStrengths,
+      this.sponsorRanking,
       required this.github,
-      required this.resume,
-      required this.design,
-      required this.website,
-      required this.essays,
-      required this.dietaryRestrictions,
-      required this.shirtSize,
-      required this.wantsHardware,
-      required this.address,
-      required this.region,
+      this.resume,
+      this.design,
+      this.website,
+      this.essays,
+      this.dietaryRestrictions,
+      this.shirtSize,
+      this.wantsHardware,
+      this.address,
+      this.region,
       required this.displayName,
-      required this.profilePicture
-  });
+      this.profilePicture});
 
   factory Profile.fromJson(Map<String, dynamic> parsedJson) {
     return Profile(
         totalPoints: parsedJson['totalPoints'],
         user: parsedJson['user'],
         event: parsedJson['event'],
-        email: parsedJson['email'],
         firstName: parsedJson['firstName'],
         lastName: parsedJson['lastName'],
         age: parsedJson['age'],
         school: parsedJson['school'],
         college: parsedJson['college'],
-        level: parsedJson['level'],
+        level: parsedJson['collegeLevel'],
         graduationYear: parsedJson['graduationYear'],
         gender: parsedJson['gender'],
         genderOther: parsedJson['genderOther'],
@@ -112,7 +108,6 @@ class Profile {
         address: parsedJson['address'],
         region: parsedJson['region'],
         displayName: parsedJson['displayName'],
-        profilePicture: parsedJson['profilePicture']
-    );
+        profilePicture: parsedJson['profilePicture']);
   }
 }
