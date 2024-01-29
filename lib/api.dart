d6520cdd2c48a51c112da703822d505a2af6bfd5
Anuda Weerasinghe <anuda@live.com>
2024-01-28 21:12:53 -0500

    fixes check in confirmation dialog text color

--- lib/api.dart
+++ lib/api.dart
@@ -20,7 +20,7 @@ import 'package:http_parser/http_parser.dart';
 
 late SharedPreferences prefs;
 
-const baseUrl = "https://backend.tartanhacks.com/";
+const baseUrl = "https://dev.backend.tartanhacks.com/";
 
 Future<User?> checkCredentials(String email, String password) async {
   Uri url = Uri.parse("${baseUrl}auth/login");
